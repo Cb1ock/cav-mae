2317a209d34bf046d2b13cc82fc0a0a2b9220b24
Cb1ock <ch3197@qq.com>
2024-04-05 18:12:10 +0800

    Refactor code: deleted sample_video_extract_list.csv, updated .gitignore, and modified cav_mae.py and train_example.py

--- src/preprocess/extract_video_frame.py
+++ src/preprocess/extract_video_frame.py
@@ -12,10 +12,13 @@ from PIL import Image
 import torchvision.transforms as T
 from torchvision.utils import save_image
 
+from multiprocessing import Pool
+
 preprocess = T.Compose([
     T.Resize(224),
     T.CenterCrop(224),
     T.ToTensor()])
+
 
 def extract_frame(input_video_path, target_fold, extract_frame_num=10):
     # TODO: you can define your own way to extract video_id
@@ -27,31 +30,44 @@ def extract_frame(input_video_path, target_fold, extract_frame_num=10):
     total_frame_num = min(int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)), int(fps * 10))
     for i in range(extract_frame_num):
         frame_idx = int(i * (total_frame_num/extract_frame_num))
-        print('Extract frame {:d} from original frame {:d}, total video frame {:d} at frame rate {:d}.'.format(i, frame_idx, total_frame_num, int(fps)))
+        #print('Extract frame {:d} from original frame {:d}, total video frame {:d} at frame rate {:d}.'.format(i, frame_idx, total_frame_num, int(fps)))
         vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx - 1)
         _, frame = vidcap.read()
         cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         pil_im = Image.fromarray(cv2_im)
         image_tensor = preprocess(pil_im)
         # save in 'target_path/frame_{i}/video_id.jpg'
-        if os.path.exists(target_fold + '/frame_{:d}/'.format(i)) == False:
-            os.makedirs(target_fold + '/frame_{:d}/'.format(i))
-        save_image(image_tensor, target_fold + '/frame_{:d}/'.format(i) + video_id + '.jpg')
+        if os.path.exists(target_fold + f'/frame_{i}') == False:
+            os.makedirs(target_fold + f'/frame_{i}')
+        save_image(image_tensor, target_fold +f'/frame_{i}' +f'/{video_id}' + '.jpg')
 
+def process_video(video_file, target_fold):
+    #print(f'Processing video {video_file}...')
+    
+    
+    extract_frame(video_file, target_fold)
+    
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser(description="Python script to extract frames from a video, save as jpgs.")
-    parser.add_argument("-input_file_list", type=str, default='sample_video_extract_list.csv', help="Should be a csv file of a single columns, each row is the input video path.")
-    parser.add_argument("-target_fold", type=str, default='./sample_frames/', help="The place to store the video frames.")
+    parser.add_argument("-input_file_list", type=str, default='/home/chenghao/Project/cav-mae/src/preprocess/celebvtext_video_list.csv', help="Should be a csv file of a single columns, each row is the input video path.")
+    parser.add_argument("-target_fold", type=str, default='/data/public_datasets/CelebV-Text/video/frames', help="The place to store the video frames.")
     args = parser.parse_args()
 
     # note the first row (header) is skipped
     input_filelist = np.loadtxt(args.input_file_list, dtype=str, delimiter=',')
+
+    file_path = '/data/public_datasets/CelebV-Text/video/celebvtext_video/'
+    
     num_file = input_filelist.shape[0]
-    print('Total {:d} videos are input'.format(num_file))
-    for file_id in range(num_file):
-        try:
-            print('processing video {:d}: {:s}'.format(file_id, input_filelist[file_id]))
-            extract_frame(input_filelist[file_id], args.target_fold)
-        except:
-            print('error with ', print(input_filelist[file_id]))
+    #print(f'Total {num_file} videos are input')
+
+    num_cores = os.cpu_count()
+    # create a multiprocessing Pool
+    pool = Pool(processes=num_cores // 2)
+    video_paths = [(file_path + input_file, args.target_fold) for input_file in input_filelist]
+
+    # 打印使用的进程数量
+    print(f"使用了 {pool._processes} 个进程")
+
+    pool.starmap(process_video, video_paths)
